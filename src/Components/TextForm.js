0bcd79900f3133669e7c614a07645aafcfbd9ea7
Karan Soni <Karanssoni2002@gmail.com>
2024-09-04 19:49:34 +1000

    fixed popup margin issue

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -33,7 +33,7 @@ export default function TextForm(props) {
   }
   return (
     <>
-    <div>
+    <div className='mt-5'>
         <h1  style ={{color: props.mode==='dark'?'white':'#042743'}}>{props.heading}</h1>
         <div className="mb-3">
         <textarea className="form-control my-2" value = {text} onChange={handleOnChange} style ={{backgroundColor: props.mode==='light'?'white':'#042743', color: props.mode === 'light'?'black':'white'}} id="myBox" rows="7" ></textarea>
